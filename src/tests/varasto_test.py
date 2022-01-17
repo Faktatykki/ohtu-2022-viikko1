e3fdc899e1af8a692336b1e2acf304bdea341741
Sampo Kääriäinen <sampo.kaariainen@gmail.com>
2022-01-17 16:12:34 +0200

    osa 8

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,49 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_tilavuus_ei_alle_nollan(self):
+        nolla_varasto = Varasto(-2, 0)
+
+        self.assertAlmostEqual(nolla_varasto.tilavuus, 0)
+        
+
+    def test_saldo_ei_alle_nollan(self):
+        self.varasto.lisaa_varastoon(10)
+        self.varasto.ota_varastosta(40)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_saldo_ei_ylita_tilavuutta(self):
+        self.varasto.lisaa_varastoon(500)
+
+        self.assertAlmostEqual(self.varasto.saldo, self.varasto.tilavuus)
+
+    def test_alle_nollan_lisaaminen_ei_muuta(self):
+        self.varasto.lisaa_varastoon(-10)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_alle_nollan_ottaminen_ei_vaikuta(self):
+        self.varasto.lisaa_varastoon(8)
+
+        self.varasto.ota_varastosta(-6)
+
+        self.assertAlmostEqual(self.varasto.saldo, 8)
+
+    def test_saldo_alle_nollan_menee_nollaan(self):
+        testi_varasto = Varasto(10, -5)
+
+        self.assertAlmostEqual(testi_varasto.saldo, 0)
+
+    def test_to_string_antaa_oikeat_arvot(self):
+        self.varasto.lisaa_varastoon(5)
+
+        varasto2 = Varasto(10)
+
+        varasto2.lisaa_varastoon(5)
+
+        self.assertTrue(str(self.varasto) == str(varasto2))
+        
+
+        
